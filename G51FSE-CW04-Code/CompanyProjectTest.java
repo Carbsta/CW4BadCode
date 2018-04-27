53f6f530076471962db639626b10196441134861
Thomas Dudley <psytcd@ad.nottingham.ac.uk>
2018-04-27 12:44:07 +0100

    Initial Commit.

---
+++ G51FSE-CW04-Code/CompanyProjectTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CompanyProjectTest {
+	//Tests by Chris, Audrey and Jacob
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
