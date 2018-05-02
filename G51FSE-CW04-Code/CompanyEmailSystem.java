71ca158f12c38fe009259aea2864ea0105b6417f
Liam James Hewer <psyljh@linode024.cs.nott.ac.uk>
2018-05-02 14:10:52 +0100

    rewrote CompanyEmailSystem to be Object Oriented for testing.

--- G51FSE-CW04-Code/CompanyEmailSystem.java
+++ G51FSE-CW04-Code/CompanyEmailSystem.java
@@ -5,12 +5,17 @@ public class CompanyEmailSystem {
 	public static int GlobalProjectCounter;
 	public static String[] ProjectPhases = new String[]{"Feasibility","Design","Implementation","Testing","Deployment","Completed"};
 	
-    private static ArrayList<CompanyProject> AllProjects;
-    private static int currentProjShowing;
+    private ArrayList<CompanyProject> AllProjects;
+    private int currentProjShowing;
     
     public static void main(String[] args) {
-        
-        ///////
+    	
+    	new CompanyEmailSystem();
+    	
+    }
+    	
+    public CompanyEmailSystem() { 
+  ///////
         //Startup
         //////
         GlobalProjectCounter = 0;
@@ -93,10 +98,9 @@ public class CompanyEmailSystem {
             }
         }
         in.close();
-        
     }
     
-    private static void ListProjects(){
+    private void ListProjects(){
         for (int x = 0; x < AllProjects.size(); x++) {
             CompanyProject cp = AllProjects.get(x);
             int emailCount = cp.getEmailsForPhase().size();
@@ -104,7 +108,7 @@ public class CompanyEmailSystem {
         }
     }
     
-    private static void AddProject(Scanner in) {
+    private void AddProject(Scanner in) {
         System.out.println("What is the title of the project?");
         in.nextLine(); // to remove read-in bug
         String title = in.nextLine();
@@ -112,7 +116,7 @@ public class CompanyEmailSystem {
         System.out.println("[Project added]");
     }
     
-    private static void ListEmails(int phaseToShow) {
+    private void ListEmails(int phaseToShow) {
         CompanyProject cp = AllProjects.get(currentProjShowing);
         ArrayList<CompanyEmail> projectPhaseEmails = null;
         if (phaseToShow==0) {
@@ -137,14 +141,14 @@ public class CompanyEmailSystem {
         }
     }
     
-    private static void ListPhases() {
+    private void ListPhases() {
         CompanyProject cp = AllProjects.get(currentProjShowing);
         for (int x=0; x < cp.getPhaseByID(); x++ ) {
             System.out.println((x+1)+") "+cp.getPhaseByName()+" - "+cp.getEmailsForPhase(x).size()+" Emails");
         }
     }
     
-    private static void ListContacts() {
+    private void ListContacts() {
         CompanyProject cp = AllProjects.get(currentProjShowing);
         ArrayList<String> projectContacts = cp.getProjectContacts();
         for (int x=0; x < projectContacts.size(); x++ ) {
@@ -152,7 +156,7 @@ public class CompanyEmailSystem {
         }
     }
     
-    private static void AddEmail(Scanner in) {
+    private void AddEmail(Scanner in) {
         System.out.println("Which email address is it from?");
         in.nextLine(); //to remove read-in bug
         String fromAddress = in.nextLine();
@@ -168,7 +172,7 @@ public class CompanyEmailSystem {
         System.out.println("[Email added to " + cp.toString() + "]");
     }
     
-    private static void ChangeProjectPhase() {
+    private void ChangeProjectPhase() {
         CompanyProject cp = AllProjects.get(currentProjShowing);
         if (cp.nextPhase()) {
             System.out.println("[Phase changed: " + cp.toString());
--- G51FSE-CW04-Code/CompanyEmailSystemTest.java
+++ G51FSE-CW04-Code/CompanyEmailSystemTest.java
@@ -1,13 +1,37 @@
 import static org.junit.Assert.*;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
-public class CompanyEmailSystemTest {
-	
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+public class CompanyEmailSystemTest {
+
+
+		
+		private final ByteArrayOutputStream outContent = new
+				ByteArrayOutputStream();
+		
+		@Before
+		 public void setUpStreams() {
+		 System.setOut(new PrintStream(outContent));
+		 }
+		
+		@After
+		 public void cleanUpStreams() {
+		 System.setOut(null);
+		 }
+		
+		@Test
+		 public void testMain() {
+		 CompanyEmailSystem ces = new CompanyEmailSystem();
+		 ces.main(null);
+		 assertEquals("What do you want to do?\\n P = List [P]rojects, [num] = Open Project [num], A = [A]dd Project, X = E[x]it",outContent.toString());
+		 }
 
 }
